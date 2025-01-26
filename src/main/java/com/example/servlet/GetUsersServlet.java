df84249614b482ae8c5efcb854b10dd1cdbc1ce9
Nikita Pyshchev <npyshchev@igatec.com>
2025-01-26 18:28:46 +0300

    add servlets add and users to work with them

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,20 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("users", Warehouse.getInstance().getUsers());
+        req.getRequestDispatcher("jsp/users.jsp").forward(req, resp);
+    }
 }
